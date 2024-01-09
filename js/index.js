d7f0aaa3da4f3cd069435b3322849e982fc929c0
MiaTexnes <miatex50428@stud.noroff.no>
2024-01-09 20:28:13 +0100

    09.01.24

--- js/index.js
+++ js/index.js
@@ -1,28 +1,31 @@
-const url = "https://api.noroff.dev/api/v1/rainy-days";
+import { url } from "./constants.js";
 
 async function getProducts() {
   try {
     const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
+
+    if (response.ok !== true) {
+      throw new Error(`HTTP Error! status: ${response.status}`);
     }
+
     const products = await response.json();
 
     const resultsContainer = document.querySelector("#container-product");
     resultsContainer.innerHTML = "";
 
     const firstThreeProducts = products.slice(0, 3);
+
     firstThreeProducts.forEach(function (product) {
       resultsContainer.innerHTML += `<div class="card">
-        <h1>${product.title}</h1>
-        <p>Price: ${product.price}</p>
-        <img src="${product.image}" alt="${product.description}" />
-        <a href="product.html"?id=${product.id}>View details</a>
-      </div>`;
+    <h1>${product.title}</h1>
+    <p>Price: ${product.price}</p>
+    <img src="${product.image}" alt="${product.description}" />
+    <a href="product.html?id=${product.id}">View details</a>
+  </div>`;
     });
   } catch (error) {
     console.error("Error fetching products:", error);
-    // Optionally, update the UI to notify the user that an error has occurred
+
     const resultsContainer = document.querySelector("#container");
     resultsContainer.innerHTML = `<p>Failed to load products. Please try again later.</p>`;
   }
--- js/product.js
+++ js/product.js
@@ -1,23 +1,61 @@
-const url = "https://api.noroff.dev/api/v1/rainy-days";
+import { getQueryStringParam } from "./helpers/getQueryStringParam.js";
+import { url } from "./constants.js";
 
 async function getProduct() {
-  const response = await fetch(url);
-  const product = await response.json();
-  // console.log(response);
+  const id = getQueryStringParam("id");
 
-  const resultsContainer = document.querySelector("#container-product");
+  if (!id) {
+    document.location.href = "/";
+  }
 
-  resultsContainer.innerHTML = "";
+  const productUrl = `${url}/${"07a7655a-7927-421b-ba6a-b6742d5a75b8"}`;
 
-  const firstProduct = product.slice(0, 1);
-  firstProduct.forEach(function (product) {
-    resultsContainer.innerHTML += `<div class="card" >
+  try {
+    const response = await fetch(productUrl);
+
+    if (response.ok === false) {
+      throw new Error("There was an error fetching the product with id: " + id);
+    }
+
+    console.log(response);
+
+    const product = await response.json();
+    console.log(product);
+
+    const resultsContainer = document.querySelector("#container-product");
+
+    resultsContainer.innerHTML = `<div class="card">
       <h1>${product.title}</h1>
-      <p>Price:${product.price}</p>
+      <p>Price: ${product.price}</p>
       <img src="${product.image}" alt="${product.description}" />
       <h2>${product.description}</h2>
-    </div>`;
-  });
+      </div>`;
+  } catch (error) {
+    console.log(error);
+    const resultsContainer = document.querySelector("#container-product");
+    resultsContainer.innerHTML = `<p class="error">${error}</p>`;
+  }
 }
 
 getProduct();
+// async function getProduct() {
+//   const response = await fetch(url);
+//   const product = await response.json();
+//   // console.log(response);
+
+//   const resultsContainer = document.querySelector("#container-product");
+
+//   resultsContainer.innerHTML = "";
+
+//   const firstProduct = product.slice(0, 1);
+//   firstProduct.forEach(function (product) {
+//     resultsContainer.innerHTML += `<div class="card" >
+//       <h1>${product.title}</h1>
+//       <p>Price:${product.price}</p>
+//       <img src="${product.image}" alt="${product.description}" />
+//       <h2>${product.description}</h2>
+//     </div>`;
+//   });
+// }
+
+// getProduct();
